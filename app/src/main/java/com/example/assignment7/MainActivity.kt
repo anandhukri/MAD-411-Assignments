1ba2882dd3db7ac21ac850ed6e8ff70b9720fadb
Anandhukrishnan <w0852083@myscc.ca>
2025-02-26 09:07:06 -0800

    Assignment 6 done

--- app/src/main/java/com/example/assignment7/MainActivity.kt
+++ app/src/main/java/com/example/assignment7/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.assignment7
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var nameEdit: EditText
+    lateinit var result: TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        nameEdit = findViewById(R.id.nameEdit)
+        result = findViewById(R.id.result)
+
+    }
+
+    fun display(view: View){
+        val name = nameEdit.text.toString()
+        result.text = "Hello $name!"
     }
 }
