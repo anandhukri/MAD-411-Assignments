966131618d1b9bed674a984d769698c76a90884c
Anandhukrishnan <w0852083@myscc.ca>
2025-04-04 13:11:43 -0700

    final code. not working

--- app/src/main/java/com/example/assignment7/HeaderFragment.kt
+++ app/src/main/java/com/example/assignment7/HeaderFragment.kt
@@ -26,13 +26,7 @@ class HeaderFragment : Fragment() {
 
     companion object {
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HeaderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+
     }
 }
