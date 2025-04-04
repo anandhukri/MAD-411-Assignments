0c248cbea6f5dd4864902604b46577b075760536
Anandhukrishnan <w0852083@myscc.ca>
2025-04-04 12:57:48 -0700

    tryin

--- app/src/main/java/com/example/assignment7/QuoteApiService.kt
+++ app/src/main/java/com/example/assignment7/QuoteApiService.kt
@@ -3,4 +3,5 @@ package com.example.assignment7
 interface QuoteApiService {
         @GET("random")
         suspend fun getQuotes(): List<Quote>
+        abstract fun getExchangeRates(s: String): Any
 }
