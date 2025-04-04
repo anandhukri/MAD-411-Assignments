7dd66bb4d507dca01cb754c16fcab942b1d57495
Anandhukrishnan <w0852083@myscc.ca>
2025-04-03 19:07:48 -0700

    finisheed the assignment 9

--- app/src/main/java/com/example/assignment7/MainActivity.kt
+++ app/src/main/java/com/example/assignment7/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.Calendar
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var editTextExpenseName: EditText
     lateinit var editTextAmount: EditText
